d4f5046e8bb6db7a5d7f7e6825643ac1883d16ab
webashraf <aliashraftamim@gmail.com>
2023-03-28 16:55:28 +0600

    add local product add

--- src/comonents/Shop/Shop.jsx
+++ src/comonents/Shop/Shop.jsx
@@ -1,52 +1,94 @@
-import React, { useEffect, useState } from 'react';
-import Product from '../Products/Product';
-import './Shop.css'
+import React, { useEffect, useState } from "react";
+import { addToDb } from "../../utilities/fakedb";
+import Product from "../Products/Product";
+import "./Shop.css";
 
 const Shop = () => {
-          const [product, setProduct] = useState([]);
-          useEffect( () =>{
-                    fetch('products.json')
-                    .then(res => res.json())
-                    .then(data => setProduct(data))
-          } ,[])
+  const [product, setProduct] = useState([]);
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProduct(data));
+  }, []);
 
-          const [cart, setCart] = useState([]);
-          const handleAddToCart = (product) =>{
-                    const newCart =[...cart, product];
-                    setCart(newCart);
-          }
+  const [cart, setCart] = useState([]);
+  const handleAddToCart = (product) => {
+    const newCart = [...cart, product];
+    setCart(newCart);
+    addToDb(product.id);
+  };
 
-          return (
-                    <div className='shop-parents'>
-                            <div className="product grid grid-cols-4 gap-7">
-                              {
-                              product.map(p => <Product 
-                                        key={p.id}
-                                        products={p}
-                                        handleAddToCart={handleAddToCart}
-                              >
+  let total = 0;
+  let shipping = 0;
+  let tax = 0;
+  // console.log(product);
+  for (const item of cart) {
+    console.log(item);
+    total = total + item.price;
+    shipping = shipping + item.shipping;
+    tax = (total * 0.1).toFixed(2);
+  }
+  // console.log(shipping);
+  return (
+    <div className="shop-parents max-h-[1200px]">
+      <div className="product grid grid-cols-4 gap-7">
+        {product.map((p) => (
+          <Product
+            key={p.id}
+            products={p}
+            handleAddToCart={handleAddToCart}
+          ></Product>
+        ))}
+      </div>
 
-                              </Product>)
-                              }
-                            </div>
-                            <div className="order-summary bg-orange-200 p-5 text-black">
-                              <h3 className='text-black text-3xl'>Order Summary</h3>
-                              <h4 className='text-lg mb-2'>Selected Items: {cart.length}</h4>
-                              <h4 className='text-lg mb-2'>Total Price: $1140</h4>
-                              <h4 className='text-lg mb-2'>Total Shipping Charge: $5</h4>
-                              <h4 className='text-lg mb-2'>Tax: $114</h4>
-                              <h4 className='text-3xl mb-2'>Grand Total: $1559</h4>
-                              <button class="btn gap-2 bg-red-600 border-red-500 btn-block mb-3">
-  Button
-  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
-</button>
-<button class="btn gap-2 bg-orange-400 border-orange-400 btn-block">
-  Button
-  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
-</button>
-                              </div>  
-                    </div>
-          );
+      <div className="order-summery">
+        <div className=" bg-orange-200 p-5 text-black sticky top-2 p-7 rounded-md">
+          <h3 className="text-black text-3xl">Order Summary</h3>
+          <h4 className="text-lg mb-2">Selected Items: {cart.length}</h4>
+          <h4 className="text-lg mb-2">Total Price: ${total}</h4>
+          <h4 className="text-lg mb-2">Total Shipping Charge: ${shipping}</h4>
+          <h4 className="text-lg mb-2">Tax: ${tax}</h4>
+          <h4 className="text-xl mb-2">
+            Grand Total: ${total + shipping + tax}
+          </h4>
+          <button className="btn gap-2 bg-red-600 border-red-500 btn-block mb-3">
+            Button
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+              />
+            </svg>
+          </button>
+          <button className="btn gap-2 bg-orange-400 border-orange-400 btn-block">
+            Button
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;

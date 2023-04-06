1589ac8aa2d538b96548595e5e88c10bb93a437e
webashraf <aliashraftamim@gmail.com>
2023-04-06 16:43:57 +0600

    add delete functionality

--- src/comonents/Order/Order.jsx
+++ src/comonents/Order/Order.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import '../GLOBALCSS/global.css'
 import { useLoaderData } from 'react-router-dom';
 import Rivew from '../Rivew/Rivew';
+import { removeFromDb } from '../../utilities/fakedb';
 
 const Order = () => {
-    const cart = useLoaderData();
-    console.log(cart);
+    const SavedCart = useLoaderData();
+    const [cart, setCart] = useState(SavedCart)
+    const handleDeleteBtn = id =>{
+        console.log(id);
+        const remmeiningProduct = cart.filter(product => product.id !== id);
+        setCart(remmeiningProduct);
+        removeFromDb(id)
+    }
+
+    console.log(SavedCart);
     return (
         <div className='global-order'>
             <div className='order-section'>
                     {
-                        cart.map(product => <Rivew key={product.id} products={product}></Rivew>)
+                        cart.map(product => <Rivew key={product.id} products={product} handleDeleteBtn={handleDeleteBtn}></Rivew>)
                     }
                 </div>
 
--- src/comonents/Rivew/Rivew.jsx
+++ src/comonents/Rivew/Rivew.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 // import Rivew from './Rivew';
 
-const Rivew = ({ products }) => {
+const Rivew = ({ products, handleDeleteBtn }) => {
   console.log(products);
   return (
     <div className="flex gap-5 border border-2 items-center mt-3">
@@ -17,7 +17,7 @@ const Rivew = ({ products }) => {
       </div>
       <div className="ml-auto pr-4">
         <div className="bg-red-200 w-14 h-14 flex items-center justify-center rounded-full">
-          <button>
+          <button onClick={() => handleDeleteBtn(products.id)}>
             <FontAwesomeIcon className="text-red-600 text-3xl" icon={faTrash} />
           </button>
         </div>

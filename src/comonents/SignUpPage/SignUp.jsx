a799510db327778b114437bfde135bf6e5cfe867
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2023-04-16 23:54:37 +0600

    add login and sign up form

---
+++ src/comonents/SignUpPage/SignUp.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const SignUp = () => {
+    return (
+        <div className="border-2">
+        <div className="hero min-h-screen bg-base-200">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="text-center lg:text-left w-1/3">
+              <h1 className="text-5xl font-bold">Sign Up now!</h1>
+              <p className="py-6">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+                excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+                et a id nisi.
+              </p>
+            </div>
+            <div className="card flex-shrink-0 w-full max-w-lg shadow-2xl bg-base-100 py-8">
+              <form className="card-body">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    required
+                    placeholder="email"
+                    className="input input-bordered"
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    required
+                    placeholder="password"
+                    className="input input-bordered"
+                  />
+                  <label className="label">
+                    <a href="#" className="label-text-alt link link-hover">
+                      Forgot password?
+                    </a>
+                  </label>
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Confirm Password</span>
+                  </label>
+                  <input
+                    type="password"
+                    name="confirm"
+                    required
+                    placeholder="Confirm password"
+                    className="input input-bordered"
+                  />
+                  <label className="label">
+                    <a href="#" className="label-text-alt link link-hover">
+                      Forgot password?
+                    </a>
+                  </label>
+                </div>
+                <div className="form-control mt-6">
+                  <button className="btn btn-primary">Login</button>
+                </div>
+                <span className="text-center">
+                  Already have an account?{" "}
+                  <Link className="btn-link" to={"/login"}>
+                    Log In
+                  </Link>
+                </span>
+              </form>
+              <span className=" w-5/6 mx-auto mb-5 flex justify-center items-center gap-4">
+                {" "}
+                <hr className="w-1/2 inline-block" />{" "}
+                <p className="text-2xl">or</p>{" "}
+                <hr className="inline-block w-1/2" />
+              </span>
+              <div className="flex items-center gap-5  justify-center w-5/6 border py-2 rounded-xl mx-auto">
+                <img
+                  className="w-1/12"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/706px-Google_%22G%22_Logo.svg.png"
+                  alt=""
+                />
+                <span className="text-xl">Continue with google</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default SignUp;
--- src/main.jsx
+++ src/main.jsx
@@ -1,49 +1,57 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import App from './App'
-import Checkout from './comonents/Checkout/Checkout'
-import cardLoaderData from './comonents/JS/cardLoaderData'
-import Order from './comonents/Order/Order'
-import Shop from './comonents/Shop/Shop'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import Checkout from "./comonents/Checkout/Checkout";
+import cardLoaderData from "./comonents/JS/cardLoaderData";
+import Order from "./comonents/Order/Order";
+import Shop from "./comonents/Shop/Shop";
+import "./index.css";
+import Login from "./comonents/LoginPage/Login";
+import SignUp from "./comonents/SignUpPage/SignUp";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children:[
+    children: [
       {
-        path: '/',
+        path: "/",
         element: <Shop />,
       },
       {
-        path: 'order',
+        path: "order",
         element: <Order />,
         loader: cardLoaderData,
+      },
+      {
+        path: "morder",
+        element: <div>Manage Order page</div>,
+      },
+      {
+        path: "inventory",
+        element: <div>Inventory page</div>,
+      },
+      {
+        path: "checkout",
+        element: <Checkout></Checkout>,
+      },
+      { 
+        path: "login",
+        element: <Login></Login>,
+      },
+      { 
+        path: "signin",
+        element: <SignUp></SignUp>,
+      },
 
-      },
-      {
-        path: 'morder',
-        element: <div>Manage Order page</div>
-      },
-      {
-        path: 'inventory',
-        element: <div>Inventory page</div>
-      },
-      {
-path: 'checkout',
-element: <Checkout></Checkout>
-      },
-      {
-        path: 'login',
-        element: <div>Login page</div>
-      },
-    ]
+    ],
   },
   {
-    path: '*',
-    element: <div>Error page</div>
-  }
+    path: "*",
+    element: <div>Error page</div>,
+  },
 ]);
-ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
